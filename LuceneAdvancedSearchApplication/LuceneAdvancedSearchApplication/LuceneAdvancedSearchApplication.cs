b0123a9de5d32fead2d5ba1dfcf58938afa7b583
JamesC <JamesC@DESKTOP-TIB9AS0>
2018-09-28 16:12:24 +1000

    Revise the IndexText function and batch add the Crandoc files to the indexes.

--- LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication.cs
+++ LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication.cs
@@ -10,6 +10,7 @@ using Lucene.Net.Store; //for Directory
 using Lucene.Net.Search; // for IndexSearcher
 using Lucene.Net.QueryParsers;  // for QueryParser
 using Lucene.Net.Analysis.Snowball; //hahahahaha
+using System.IO;
 
 namespace LuceneAdvancedSearchApplication
 {
@@ -33,7 +34,7 @@ namespace LuceneAdvancedSearchApplication
             analyzer = new Lucene.Net.Analysis.Snowball.SnowballAnalyzer(VERSION, "English");      // Stop analyzer takes 
             parser = new QueryParser(Lucene.Net.Util.Version.LUCENE_30, TEXT_FN, analyzer);
             newSimilarity = new NewSimilarity();
-            
+
         }
         /// <summary>
         /// Creates the index at a given path
@@ -41,24 +42,12 @@ namespace LuceneAdvancedSearchApplication
         /// <param name="indexPath">The pathname to create the index</param>
         public void CreateIndex(string indexPath)
         {
+
             luceneIndexDirectory = Lucene.Net.Store.FSDirectory.Open(indexPath);
             IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
             // writer = new Lucene.Net.Index.IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
             writer = new Lucene.Net.Index.IndexWriter(luceneIndexDirectory, analyzer, true, mfl);
             //writer.SetSimilarity(newSimilarity);
-        }
-
-        /// <summary>
-        /// Indexes a given string into the index
-        /// </summary>
-        /// <param name="text">The text to index</param>
-        public void IndexText(string text)
-        {
-
-            Lucene.Net.Documents.Field field = new Field(TEXT_FN, text, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES);
-            Lucene.Net.Documents.Document doc = new Document();
-            doc.Add(field);
-            writer.AddDocument(doc);
         }
 
         /// <summary>
@@ -94,7 +83,7 @@ namespace LuceneAdvancedSearchApplication
 
             TopDocs results = searcher.Search(query, 100);
             System.Console.WriteLine("Number of results is " + results.TotalHits);
-             int rank = 0;
+            int rank = 0;
             foreach (ScoreDoc scoreDoc in results.ScoreDocs)
             {
                 rank++;
@@ -115,8 +104,62 @@ namespace LuceneAdvancedSearchApplication
         {
             searcher.Dispose();
         }
+        //public void IndexText(string text)
+        //{
 
+        //    Lucene.Net.Documents.Field field = new Field(TEXT_FN, text, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES);
+        //    Lucene.Net.Documents.Document doc = new Document();
+        //    doc.Add(field);
+        //    writer.AddDocument(doc);
+        //}
 
+        /// <summary>
+        /// Indexes a given string into the index
+        /// </summary>
+        /// <param name="text">The text to index</param>
+        public void IndexText(string path)
+        {
+            System.IO.DirectoryInfo root = new System.IO.DirectoryInfo(path);   // Create DirectoryInfo object
+            System.IO.FileInfo[] files = null;  // Create FileInfo array
+
+            // Get all files in the directory
+            try
+            {
+                files = root.GetFiles("*.*");
+            }
+
+            catch (UnauthorizedAccessException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+
+            catch (System.IO.DirectoryNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (files != null)
+            {
+                //Console.WriteLine(files.Length);
+                //Process every file
+                foreach (System.IO.FileInfo fi in files)
+                {
+                    string name = fi.FullName;    // Get file name
+                    Console.WriteLine("Adding doc " + name + " to Index");
+
+                    StreamReader reader = new StreamReader(name);   // Create a reader
+                    string text = reader.ReadToEnd();   // Read the whole text
+
+                    //Console.WriteLine(text);
+                    Lucene.Net.Documents.Field field = new Field(TEXT_FN, text, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES);
+                    Lucene.Net.Documents.Document doc = new Document();
+                    doc.Add(field);
+                    writer.AddDocument(doc);
+                }
+
+            }
+
+        }
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello Lucene.Net");
@@ -124,35 +167,34 @@ namespace LuceneAdvancedSearchApplication
             LuceneAdvancedSearchApplication myLuceneApp = new LuceneAdvancedSearchApplication();
 
             // source collection
-            List<string> l = new List<string>();
-            l.Add("The magical world of oz");
-            l.Add("The mad, mad, mad, mad world");
-            l.Add("Possum magic");
-            l.Add("Mad isn't bad");
-            l.Add("Mad's greatest hits");
-            
+            //List<string> l = new List<string>();
+            //l.Add("The magical world of oz");
+            //l.Add("The mad, mad, mad, mad world");
+            //l.Add("Possum magic");
+            //l.Add("Mad isn't bad");
+            //l.Add("Mad's greatest hits");
+
 
             // Index code
             string indexPath = @"C:\LuceneFolder";
-            myLuceneApp.CreateIndex(indexPath);
+            string sourcePath = @"D:\Desktop\ifn647-project\LuceneAdvancedSearchApplication\crandocs";
+
+            DateTime start = System.DateTime.Now;   // Indexing time starts
+            myLuceneApp.CreateIndex(indexPath);     // Create index at the given path
             System.Console.WriteLine("Adding Documents to Index");
-            int docID = 0;
-            foreach (string s in l)
-            {
+            myLuceneApp.IndexText(sourcePath);      // Add file collection to the index one by one
+            myLuceneApp.CleanUpIndexer();           // Flush the buffer and close the index
+            Console.WriteLine("All documents added.");
+            DateTime end = System.DateTime.Now;   // Indexing time ends
+            Console.WriteLine("The time for creating index was " + (end - start));  // Calculate and show the indexing time
 
-                System.Console.WriteLine("Adding doc " + docID + ". " + s + "  to Index");
-                myLuceneApp.IndexText(s);
-                docID++;
-            }
-            System.Console.WriteLine("All documents added.");
-            myLuceneApp.CleanUpIndexer();
 
-            // Searching Code
-            myLuceneApp.CreateSearcher();
-           
-            myLuceneApp.SearchText("mad");
-            
-            myLuceneApp.CleanUpSearcher();
+            //// Searching Code
+            //myLuceneApp.CreateSearcher();
+
+            //myLuceneApp.SearchText("mad");
+
+            //myLuceneApp.CleanUpSearcher();
 
 
             Console.ReadLine();
