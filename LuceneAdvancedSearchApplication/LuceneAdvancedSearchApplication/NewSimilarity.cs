d81f1b3808d975addc014b20d39745e5860ecff1
Sean <shung47@gmail.com>
2018-10-20 15:39:18 +1000

    Updated tf idf

--- LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/NewSimilarity.cs
+++ LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/NewSimilarity.cs
@@ -16,16 +16,17 @@ namespace LuceneAdvancedSearchApplication
             //1~4==>1*2 4~9 2*2, 2*2 9 16 3* increase the importance of the unique terms
             //1~4 as important as 4~8
             //4~8   2* increase the inportance of unique terms...
-
+            /*
             if (freq > 0 && freq < 4)
             {
                 freq *= 4;
             }
-            /*
+            
              * make it decrease the overall inportance of tf
              * 
              */
-            return (float)Math.Sqrt(freq) / 2;
+            return (float)(Math.Log(freq) + 1);
+            //return (float)Math.Sqrt(freq) / 2;
         }
 
         public float idf(long docFreq, long numDocs)
@@ -35,10 +36,12 @@ namespace LuceneAdvancedSearchApplication
             //if it just exist in one docs... then 
             // log not that many different between 1~13 and
             //treat 1~4 is more imfortant than 6~13
+            /*
             if (docFreq > 0 && docFreq < 5)
             {
                 docFreq *= 10;
             }
+            */
             return (float)(Math.Log(numDocs / (double)(docFreq + 1)) + 1.0);
         }
 
--- LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/SearchGUIForm.designer.cs
+++ LuceneAdvancedSearchApplication/LuceneAdvancedSearchApplication/SearchGUIForm.designer.cs
@@ -319,7 +319,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1240, 767);
+            this.ClientSize = new System.Drawing.Size(1240, 689);
             this.Controls.Add(this.SCCheckbox);
             this.Controls.Add(this.NeedsTxtbox);
             this.Controls.Add(this.advancedCheck);
